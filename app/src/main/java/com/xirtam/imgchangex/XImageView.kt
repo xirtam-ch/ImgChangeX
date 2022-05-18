f0bec0a9713977e895775b101c377e2cb970628f
xirtam-zhang <xirtam.zhang@gmail.com>
2022-05-18 17:03:37 +0800

    Implement distortion

--- app/src/main/java/com/xirtam/imgchangex/XImageView.kt
+++ app/src/main/java/com/xirtam/imgchangex/XImageView.kt
@@ -6,7 +6,6 @@ import android.graphics.Canvas
 import android.graphics.Matrix
 import android.graphics.Paint
 import android.util.AttributeSet
-import android.util.Log
 import android.view.View
 import kotlin.math.min
 import kotlin.math.sqrt
@@ -22,7 +21,6 @@ class XImageView(context: Context?, attrs: AttributeSet?) : View(context, attrs)
     private val WIDTH = 20
     private val HEIGHT = 20
     private val COUNT = (WIDTH + 1) * (HEIGHT + 1)
-    private val orig = FloatArray(COUNT * 2)
     private var verts = FloatArray(COUNT * 2)
 
     private var withLines = false
@@ -39,11 +37,9 @@ class XImageView(context: Context?, attrs: AttributeSet?) : View(context, attrs)
             paint.alpha = 0xFF
 
             if (withMesh) {
-                canvas.drawBitmapMesh(it, 4, 4, verts, 0, null, 0, null);
-                Log.d("xxxxxx", "withMesh")
+                canvas.drawBitmapMesh(it, WIDTH, HEIGHT, verts, 0, null, 0, null);
             } else {
                 canvas.drawBitmap(it, 0f, 0f, paint)
-                Log.d("xxxxxx", "no mesh")
             }
 
             if (withLines) {
@@ -91,67 +87,49 @@ class XImageView(context: Context?, attrs: AttributeSet?) : View(context, attrs)
 
     private fun initDistortion() {
         bitmap?.let { bitmap ->
-//            val bitmapWidth = bitmap.width.toFloat()
-//            val bitmapHeight = bitmap.height.toFloat()
-//            var index = 0
-//            for (y in 0..HEIGHT) {
-//                val fy = bitmapHeight * y / HEIGHT
-//                for (x in 0..WIDTH) {
-//                    val fx = bitmapWidth * x / WIDTH
-//                    verts[index * 2] = fx
-//                    orig[index * 2] = verts[index * 2]
-//                    verts[index * 2 + 1] = fy
-//                    orig[index * 2 + 1] = verts[index * 2 + 1]
-//                    index += 1
-//                }
-//            }
+            val bitmapWidth = bitmap.width.toFloat()
+            val bitmapHeight = bitmap.height.toFloat()
+            var index = 0
+            for (y in 0..HEIGHT) {
+                val fy = bitmapHeight * y / HEIGHT
+                for (x in 0..WIDTH) {
+                    val fx = bitmapWidth * x / WIDTH
+                    verts[index * 2] = fx
+                    verts[index * 2 + 1] = fy
+                    index += 1
+                }
+            }
         }
     }
 
     fun distortion() {
         withMesh = true
-//        for (i in 1..30) {
-//            Log.d("xxxxxx", i.toString())
-//            for (j in 1..30) {
-//                warp(100f * i, 100f * j)
-//            }
-//        }
-
-        val count = 4
-        val c = IntArray((count + 1) * (count + 1))
-        val v = FloatArray((count + 1) * (count + 1) * 2)
-        var k = 0
-        for (i in 0..count) {
-            var j = 0
-            while (j <= count) {
-                v[k] = (j * 400 + 100).toFloat()
-                v[k + 1] = (i * 400 + 100).toFloat()
-                j++
-                k += 2
+        bitmap?.let {
+            for (i in 0..it.width / 50) {
+                for (j in 0..it.height / 50) {
+                    warp(50f * i, 50f * j)
+                }
             }
         }
-
-        verts = v
-
         this.invalidate()
     }
 
     private fun warp(cx: Float, cy: Float) {
         var i = 0
         while (i < COUNT * 2) {
-            val dx: Float = cx - orig[i]
-            val dy: Float = cy - orig[i + 1]
+            val dx: Float = cx - verts[i]
+            val dy: Float = cy - verts[i + 1]
             val dd = dx * dx + dy * dy
             val d = sqrt(dd.toDouble()).toFloat()
 
-            val pull = 80000 / (dd * d)
+            val pull = 10000 / (dd * d)
 
             if (pull >= 1) {
                 verts[i] = cx
                 verts[i + 1] = cy
             } else {
-                verts[i] = orig[i] + dx * pull
-                verts[i + 1] = orig[i + 1] + dy * pull
+                verts[i] = verts[i] + dx * pull
+                verts[i + 1] = verts[i + 1] + dy * pull
             }
 
             i += 2
