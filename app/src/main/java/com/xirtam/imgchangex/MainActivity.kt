274aec6a8a88d98869bc08b9e39a61f65ccf6055
xirtam-zhang <xirtam.zhang@gmail.com>
2022-05-18 16:00:01 +0800

    Implement add lines

--- app/src/main/java/com/xirtam/imgchangex/MainActivity.kt
+++ app/src/main/java/com/xirtam/imgchangex/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.xirtam.imgchangex
 
+import android.graphics.Bitmap
+import android.net.Uri
 import android.os.Bundle
 import android.provider.MediaStore
 import android.view.View
+import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
@@ -10,6 +13,8 @@ import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
+    private var tmpUri: Uri? = null
+    private lateinit var saveDoc: ActivityResultLauncher<String>
     private lateinit var openDoc: ActivityResultLauncher<Array<String>>
     private lateinit var mainImg: XImageView
 
@@ -19,15 +24,46 @@ class MainActivity : AppCompatActivity() {
         mainImg = findViewById(R.id.main_img)
 
         openDoc = registerForActivityResult(ActivityResultContracts.OpenDocument()) {
-            val bitmap = MediaStore.Images.Media.getBitmap(this.contentResolver, it)
-            mainImg.setBitmap(bitmap)
+            if (it != null) {
+                val bitmap = MediaStore.Images.Media.getBitmap(this.contentResolver, it)
+                tmpUri = it
+                mainImg.setBitmap(bitmap)
+            } else {
+                Toast.makeText(this, "取消选择", Toast.LENGTH_SHORT).show()
+            }
+
+        }
+
+        saveDoc = registerForActivityResult(ActivityResultContracts.CreateDocument()) {
+
+            val output = contentResolver.openOutputStream(it)
+            output?.bufferedWriter().use {
+                mainImg.convertToBitmap()?.compress(Bitmap.CompressFormat.JPEG, 80, output)
+            }
+            output?.flush()
+            output?.close()
         }
     }
 
     fun onClickListener(view: View) {
         if (view.id == R.id.main_open) {
             openFile()
+        } else if (view.id == R.id.main_distortion) {
+            mainImg.distortion()
+        } else if (view.id == R.id.main_with_line) {
+            mainImg.addLines()
+        } else if (view.id == R.id.main_save) {
+            if (tmpUri == null) {
+                Toast.makeText(this, "还没打开图片", Toast.LENGTH_SHORT).show()
+            } else {
+                save()
+            }
         }
+    }
+
+    private fun save() {
+//        mainImg.convertToBitmap()
+        saveDoc.launch("${System.currentTimeMillis()}.jpg")
     }
 
     private fun openFile() {
--- app/src/main/java/com/xirtam/imgchangex/XImageView.kt
+++ app/src/main/java/com/xirtam/imgchangex/XImageView.kt
@@ -6,23 +6,78 @@ import android.graphics.Canvas
 import android.graphics.Matrix
 import android.graphics.Paint
 import android.util.AttributeSet
+import android.util.Log
 import android.view.View
 import kotlin.math.min
+import kotlin.math.sqrt
 
 
 class XImageView(context: Context?, attrs: AttributeSet?) : View(context, attrs) {
 
+
     private var bitmap: Bitmap? = null
     private var paint: Paint = Paint()
+
+    private var withMesh = false
+    private val WIDTH = 20
+    private val HEIGHT = 20
+    private val COUNT = (WIDTH + 1) * (HEIGHT + 1)
+    private val orig = FloatArray(COUNT * 2)
+    private var verts = FloatArray(COUNT * 2)
+
+    private var withLines = false
+    var interval = 15
+    var lineWidth = 2
 
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
         bitmap?.let {
-            canvas.drawBitmap(it, (width - it.width) / 2f, 0f, paint)
+            canvas.translate((width - it.width) / 2f, 0f)
+
+
+            paint.color = 0
+            paint.alpha = 0xFF
+
+            if (withMesh) {
+                canvas.drawBitmapMesh(it, 4, 4, verts, 0, null, 0, null);
+                Log.d("xxxxxx", "withMesh")
+            } else {
+                canvas.drawBitmap(it, 0f, 0f, paint)
+                Log.d("xxxxxx", "no mesh")
+            }
+
+            if (withLines) {
+
+                paint.color = 0xee2222
+                paint.alpha = 0x7f
+
+                for (i in 0..it.height / interval) {
+                    canvas.drawRect(
+                        0f,
+                        0f + i * interval,
+                        it.width.toFloat(),
+                        0f + i * interval + lineWidth,
+                        paint
+                    )
+                }
+
+                for (i in 0..it.width / interval / 2) {
+                    canvas.drawRect(
+                        0f + i * 2 * interval,
+                        0f,
+                        0f + i * 2 * interval + lineWidth,
+                        it.height.toFloat(),
+                        paint
+                    )
+                }
+            }
         }
     }
 
     fun setBitmap(bitmap: Bitmap) {
+        withMesh = false
+        withLines = false
+
         val scaleWight = width * 1f / bitmap.width
         val scaleHeight = height * 1f / bitmap.height
         val scale = min(scaleHeight, scaleWight)
@@ -30,5 +85,91 @@ class XImageView(context: Context?, attrs: AttributeSet?) : View(context, attrs)
         val matrix = Matrix()
         matrix.postScale(scale, scale)
         this.bitmap = Bitmap.createBitmap(bitmap, 0, 0, bitmap.width, bitmap.height, matrix, true)
+
+        initDistortion()
+    }
+
+    private fun initDistortion() {
+        bitmap?.let { bitmap ->
+//            val bitmapWidth = bitmap.width.toFloat()
+//            val bitmapHeight = bitmap.height.toFloat()
+//            var index = 0
+//            for (y in 0..HEIGHT) {
+//                val fy = bitmapHeight * y / HEIGHT
+//                for (x in 0..WIDTH) {
+//                    val fx = bitmapWidth * x / WIDTH
+//                    verts[index * 2] = fx
+//                    orig[index * 2] = verts[index * 2]
+//                    verts[index * 2 + 1] = fy
+//                    orig[index * 2 + 1] = verts[index * 2 + 1]
+//                    index += 1
+//                }
+//            }
+        }
+    }
+
+    fun distortion() {
+        withMesh = true
+//        for (i in 1..30) {
+//            Log.d("xxxxxx", i.toString())
+//            for (j in 1..30) {
+//                warp(100f * i, 100f * j)
+//            }
+//        }
+
+        val count = 4
+        val c = IntArray((count + 1) * (count + 1))
+        val v = FloatArray((count + 1) * (count + 1) * 2)
+        var k = 0
+        for (i in 0..count) {
+            var j = 0
+            while (j <= count) {
+                v[k] = (j * 400 + 100).toFloat()
+                v[k + 1] = (i * 400 + 100).toFloat()
+                j++
+                k += 2
+            }
+        }
+
+        verts = v
+
+        this.invalidate()
+    }
+
+    private fun warp(cx: Float, cy: Float) {
+        var i = 0
+        while (i < COUNT * 2) {
+            val dx: Float = cx - orig[i]
+            val dy: Float = cy - orig[i + 1]
+            val dd = dx * dx + dy * dy
+            val d = sqrt(dd.toDouble()).toFloat()
+
+            val pull = 80000 / (dd * d)
+
+            if (pull >= 1) {
+                verts[i] = cx
+                verts[i + 1] = cy
+            } else {
+                verts[i] = orig[i] + dx * pull
+                verts[i + 1] = orig[i + 1] + dy * pull
+            }
+
+            i += 2
+        }
+    }
+
+    fun addLines() {
+        withLines = true
+
+        this.invalidate()
+    }
+
+    fun convertToBitmap(): Bitmap? {
+        bitmap?.let {
+            val bmp = Bitmap.createBitmap(it.width, it.height, Bitmap.Config.ARGB_8888)
+            draw(Canvas(bmp))
+            return bmp
+        }
+        return null
     }
 }
